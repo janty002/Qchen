e2f33d4d5fa6323168b3ac7129de4356a14344ce
qchen <jantychen@gmail.com>
2014-01-13 23:46:34 -0600

    Create string1.h

---
+++ string1.h
@@ -0,0 +1,31 @@
+# include <iostream>
+using namespace std;
+# ifndef STRING__H_
+# define STRING__H_
+
+class String{
+	char* str;
+	int len;
+	static int num_strings;
+	static const int CINLIM = 80;
+public:
+	String(const char* s);
+	String();
+	String(const String&);
+	~String();
+	int length()const { return len; }
+	//overload operator methods
+	String& operator=(const String&);
+	String& operator=(const char* s);
+	char& operator[](int i);
+	const char& operator[](int i) const;
+	friend bool operator <(const String &st1, const String &st2);
+	friend bool operator >(const String &str1, const String &st2);
+	friend bool operator==(const String &st, const String &st2);
+	friend ostream& operator <<(ostream& os, const String& rg);
+	friend istream& operator >>(istream& is, String& rg);
+
+	//static function
+	static int howMany();
+};
+# endif
